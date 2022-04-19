0d36d551122d18a9230c7fdd57ba8fdc5c0ae87b
Snehil Vijay <snehilvj@outlook.com>
2022-04-20 03:27:39 +0530

    Add basic redis caching with flask-caching

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from dash import Dash
-from dash_mantine_components import Button
+from time import sleep
+
+from dash import Dash, Output, Input, html
+import dash_mantine_components as dmc
+
+from flask_caching import Cache
 
 app = Dash(__name__)
+cache = Cache(
+    app.server,
+    config={
+        "CACHE_TYPE": "RedisCache",
+        "CACHE_REDIS_HOST": "redis",
+        "CACHE_REDIS_PORT": 6379,
+    },
+)
 
-app.layout = Button("This is a button")
+app.layout = dmc.Group(
+    [
+        dmc.NumberInput(id="number", min=1),
+        html.Div(id="container"),
+    ]
+)
+
+
+@app.callback(
+    Output("container", "children"), Input("number", "value"), prevent_initial_call=True
+)
+@cache.memoize()
+def update(value):
+    if value and value >= 0:
+        sleep(value)
+        return value
+    return "Enter valid number"
+
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", debug=True)
